05ccbb49201da3b4ab94a785f62d95c1a4a2f8a4
Cristian Contreras <crontreras1@gmail.com>
2024-11-23 22:00:32 -0500

    class 13. manejo de plantillas en astro

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
  return (
-    <header className="container mx-auto p-4">
+    <header className="container max-w-screen-2xl mx-auto p-4">
         <h1>{ title }</h1>
     </header>
  )
